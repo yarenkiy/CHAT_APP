3529e9cd5ea09aa83a593c072aab585b96517277
yarenkiy <yarennkiy@outlook.com>
2023-06-27 13:32:04 +0300

    Chat app
    Real-time chat application with Socket.io and Node.js

---
+++ chat.js
@@ -0,0 +1,27 @@
+const socket = io.connect('http://127.0.0.1:3000');
+
+const sender = document.getElementById('sender')
+const message = document.getElementById('message')
+const submit = document.getElementById('submit')
+const output = document.getElementById('output')
+const feedback= document.getElementById('feedback')
+
+submit.addEventListener('click', () => {
+    socket.emit('chat', {
+        message: message.value ,
+        sender: sender.value 
+
+    })
+
+})
+socket.on('chat',data => {
+    output.innerHTML +='<p><strong>'+ data.sender +': </strong>'+ data.message +'</p>'
+   
+})
+message.addEventListener('keypress',() => {
+    socket.emit('typing',sender.value)
+})
+socket.on('typing', data => {
+    feedback.innerHTML = '<p>' + data + 'typing... </p>'
+
+})
